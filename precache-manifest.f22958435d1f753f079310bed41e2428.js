b98b569e927bc488583101351ae1130399bdaa99
Dowen <tokenyete@gmail.com>
2019-07-04 13:14:25 +0800

    Updates

---
+++ precache-manifest.f22958435d1f753f079310bed41e2428.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d73201b9cde6c7afc2a1940704e3446f",
+    "url": "/MoveAppDocumentation/index.html"
+  },
+  {
+    "revision": "ad75e7038653488335b3",
+    "url": "/MoveAppDocumentation/static/css/2.51ff7408.chunk.css"
+  },
+  {
+    "revision": "ad75e7038653488335b3",
+    "url": "/MoveAppDocumentation/static/js/2.b2d50f2f.chunk.js"
+  },
+  {
+    "revision": "e2e9ac06efa12f9c157d",
+    "url": "/MoveAppDocumentation/static/js/main.4b9ee0e8.chunk.js"
+  },
+  {
+    "revision": "878e596e0259cc1a9d56",
+    "url": "/MoveAppDocumentation/static/js/runtime~main.f0af0f90.js"
+  }
+]);
